36bc82edf6d46d1d9c85059bb468fa31c3f36717
Irene Trullén <irene.trullenmulet@ns.nl>
2025-01-24 17:25:33 +0100

    Add printing to see the games and the winner

--- app/src/main/java/com/dojo/tictactoe/TicTacToeGame.kt
+++ app/src/main/java/com/dojo/tictactoe/TicTacToeGame.kt
@@ -22,25 +22,48 @@ class TicTacToeGame {
     }
 
     fun play(position: Cell) {
+        println("PLAYER $currentPlayer:")
+
         board[position.ordinal] = currentPlayer
         switchPlayers()
+        printBoard()
     }
 
     fun getWinner(): Player {
+        var winner = Player.None
+
         winnerCombinations.forEach { (first, second, third) ->
             if (board[first.ordinal] == board[second.ordinal]
                 && board[second.ordinal] == board[third.ordinal]
             ) {
-                // There is a winner
-                return board[first.ordinal]
+                winner = board[first.ordinal]
             }
         }
-        // There is no winner
-        return Player.None
+
+        printWinner(winner)
+
+        return winner
     }
 
     private fun switchPlayers() {
         currentPlayer = if (currentPlayer == Player.X) Player.O else Player.X
     }
 
+    private fun printBoard() {
+        board.map { if (it == Player.None) " " else it.toString() }
+            .chunked(3)
+            .forEach { println(it.joinToString("|")) }
+        println()
+    }
+
+    private fun printWinner(winner: Player) {
+        // Don't print the winner if the game have not started yet
+        if (!board.all { it == board[0] }) {
+            if (winner == Player.None) {
+                println("THE GAME ENDS WITH A DRAW!")
+            } else {
+                println("PLAYER $winner WON! ")
+            }
+        }
+    }
 }
